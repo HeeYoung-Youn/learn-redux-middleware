b370f1c1f43325ae1310fce2b793b5e45a37974f
HeeYoung-Youn <yhy1@naver.com>
2021-12-09 18:31:52 +0900

    Add: redux-thunk, redux-devtools-extension
    store에 reduxThunk 적용 / thunk함수 사용해서 dispatch

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import ReduxThunk from "redux-thunk";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+);
 
 ReactDOM.render(
   <React.StrictMode>
