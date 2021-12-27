1575dfbea1eac69d05b5fbf8a439aa7ef4966b6c
HeeYoung-Youn <yhy1@naver.com>
2021-12-27 16:02:08 +0900

    Add: backend 연동
    cors문제 해결을 위한 proxy 적용 / backend 연동

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,30 +1,11 @@
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-
-// { id, title, body }
-const posts = [
-  {
-    id: 1,
-    title: "리덕스 미들웨어 학습",
-    body: "리덕스 미들웨어 직접 구현하기",
-  },
-  {
-    id: 2,
-    title: "redux-thunk 학습",
-    body: "redux-thunk 사용해서 비동기 처리 구현",
-  },
-  {
-    id: 3,
-    title: "redux-saga 학습",
-    body: "추후 redux-saga를 사용해서 비동기 처리 방법 학습",
-  },
-];
+import axios from "axios";
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get("/posts");
+  return response.data;
 };
 
 export const getPostById = async (id) => {
-  await sleep(500);
-  return posts.find((post) => post.id === id);
+  const response = await axios.get(`/posts/${id}`);
+  return response.data;
 };
